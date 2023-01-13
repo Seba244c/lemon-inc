1f0f5be6a4dbfdcb854ed4ea12a8c771e5ff8672
Noah Muntenjon <noahmuntenjon@gmail.com>
2023-01-13 14:04:55 +0100

    Inputs ig uess

--- Player/Movement.gd
+++ Player/Movement.gd
@@ -10,8 +10,8 @@ onready var animationState = animationTree.get("parameters/playback")
 
 func _process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("ui_move_right") - Input.get_action_strength("ui_move_left")
+	input_vector.y = Input.get_action_strength("ui_move_down") - Input.get_action_strength("ui_move_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
