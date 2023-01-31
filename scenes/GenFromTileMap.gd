ffea87ad3d5666ebf253396d9a51a29bb4b771d4
Sebastian Snoer <sebs@sebsa.dk>
2023-01-31 11:14:57 +0100

    Place Tiles and a bit of UI

---
+++ scenes/GenFromTileMap.gd
@@ -0,0 +1,31 @@
+extends HBoxContainer
+
+onready var floorTileSet = load("res://Assets/TilesSets/InnerTiles.tres")
+var currentTileID = -1;
+
+func tileFromTileSet(tileId: int, tileSet: TileSet):
+	var tileSetTexture = tileSet.tile_get_texture(tileId)
+	var rect = tileSet.tile_get_region(tileId)
+	var atlas = AtlasTexture.new()
+	#var texture = ImageTexture.new()
+	atlas.atlas = tileSetTexture
+	atlas.region = rect
+	return atlas
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var tiles = floorTileSet.get_tiles_ids()
+	for tile in tiles:
+		var button = Button.new()
+		button.size_flags_horizontal = SIZE_FILL
+		button.size_flags_vertical = SIZE_FILL
+		button.icon = tileFromTileSet(tile, floorTileSet)
+		button.connect("pressed", self, "_button_pressed", [tile])
+
+		add_child(button)
+
+		button.rect_min_size = Vector2(100,100)
+
+
+func _button_pressed(tileId):
+	currentTileID = tileId
--- scenes/Shop.gd
+++ scenes/Shop.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var buildMode = false
 onready var floorTiles = get_node("Floor Tiles/TileMap")
+onready var shopGUI = get_node("ShopGUI")
+onready var floorTileSelect = get_node("ShopGUI/Panel/TabContainer/Floor Tiles/ScrollContainer/HBoxContainer")
 
 func _input(event):
 	if event.is_action_pressed("BuildModeToggle"):
@@ -13,16 +15,27 @@ func _input(event):
 			player.hide()
 		else:
 			player.show()
+		shopGUI.buildMode(buildMode)
 
-func _process(delta):
+func _unhandled_input(e):
 	if(!buildMode): return
 	
+	# Non Floor Mode
+	#if e is InputEventMouseButton:
+	#	if e.button_index == 1:
+	#		var mouse_pos = get_global_mouse_position()
+	#		var cell = floorTiles.getMapMousePos(mouse_pos)
+	#		
+	#		var currentTile = floorTiles.get_cellv(cell)
+	#		floorTiles.set_cellv(cell, 1);
 	
 	
 	# Floor Mode
-	if(Input.is_mouse_button_pressed(1)):
-		var mouse_pos = get_global_mouse_position()
-		var cell = floorTiles.getMapMousePos(mouse_pos)
-		
-		var currentTile = floorTiles.get_cellv(cell)
-		floorTiles.set_cellv(cell, 1);
+	if e is InputEventMouse:
+		if e.button_mask == 1:
+			if floorTileSelect.currentTileID == -1: return
+			var mouse_pos = get_global_mouse_position()
+			var cell = floorTiles.getMapMousePos(mouse_pos)
+			
+			var currentTile = floorTiles.get_cellv(cell)
+			floorTiles.set_cellv(cell, floorTileSelect.currentTileID);
